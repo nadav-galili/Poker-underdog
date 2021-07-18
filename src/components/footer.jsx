a697095d85070f797a6df98f27b042ab79c89e4b
nadav-galili <67638018+nadav-galili@users.noreply.github.com>
2021-07-18 19:16:50 +0300

    added social icons to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,10 +2,68 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <p className="text-center border-top pt-3">
-      Poker-Underdog was created by Nadav Galili &copy;{" "}
-      {new Date().getFullYear()}
-    </p>
+    <div className="container  ">
+      <div className="d-flex justify-content-center row ">
+        <div className="logomage col">
+          <img
+            src={process.env.PUBLIC_URL + "logoNewRed.png"}
+            alt="logo"
+            className="mt-4"
+          />
+        </div>
+
+        <p className="col align-self-center ">
+          Poker-Underdog was created by <br></br>Nadav Galili &copy;{" "}
+          {new Date().getFullYear()}
+        </p>
+        <div className="col social-icons mt-2 ">
+          <p>Contact me at:</p>
+          <ul>
+            <li>
+              <a href="https://www.facebook.com/nadav.galili">
+                <img
+                  src="https://img.icons8.com/doodle/50/000000/facebook-new.png"
+                  alt="facebook"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/nadavgalili">
+                <img
+                  src="https://img.icons8.com/doodle/50/000000/twitter--v1.png"
+                  alt="twitter"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/nadav-galili/">
+                <img
+                  src="https://img.icons8.com/doodle/50/000000/linkedin--v2.png"
+                  alt="linkedin"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/nadav-galili">
+                <img
+                  src="https://img.icons8.com/doodle/50/000000/github.png"
+                  alt="github"
+                />
+              </a>
+            </li>
+
+            <li>
+              <a href="mailto:nadavg1000@gmail.com">
+                <img
+                  src="https://img.icons8.com/plasticine/50/000000/email.png"
+                  alt="email"
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 };
 
