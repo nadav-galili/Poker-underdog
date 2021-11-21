acaaa468b17e9ba7b56b7e728d5cecb0f2ae7953
“nadav <nadavg1000@gmail.com>
2021-11-21 12:22:15 +0200

    set up nodemailer

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,16 @@ const users = require("./routes/users");
 const auth = require("./routes/auth");
 const games = require("./routes/games");
 const teams = require("./routes/teams");
-const h2h=require("./routes/h2h");
+const h2h = require("./routes/h2h");
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const http = require("http").Server(app);
 const mongoose = require("mongoose");
 const path = require("path");
+// const exphbs = require("express-handlebars");
+const hbs=require('nodemailer-express-handlebars');
+const nodemailer = require("nodemailer");
 
 // mongoose
 //   .connect(
@@ -37,16 +40,55 @@ let corsOptions = {
 };
 
 app.use(cors());
-// app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
-app.use("/api/h2h",h2h);
+// app.use(cors(corsOptions));
+app.post("/info", (req, res) => {
+  async function main() {
+    const output = `
+    <p>a new game has ended${req.body.name}</p>`;
+    // let testAccount=await nodemailer.createTestAccount();
+    let transporter = nodemailer.createTransport({
+      host: "mail.poker-underdog.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "info@poker-underdog.com",
+        pass: "Emmush2016",
+      },
+    });
+    
+
+    // let info = await transporter.sendMail({
+    //   from: '"Poker-Underdog"<info@poker-underdog.com>',
+    //   to: "nadavg1000@gmail.com",
+    //   subject: "hello",
+    //   html: output,
+    // });
+    // console.log("message sent: %s", info.messageId);
+    console.log("preview URL :%s", nodemailer.getTestMessageUrl());
+    res.send(output);
+  }
+  main().catch(console.error);
+
+});
+
+//view engine setup
+// app.engine('handlebars', exphbs());
+// app.set('view engine', 'handlebars');
+
+//static folder
+app.use("/public", express.static(path.join(__dirname, "public")));
+//body parser middelware
+
+app.use("/api/h2h", h2h);
 app.use("/api/users", users);
 app.use("/api/auth", auth);
 app.use("/api/games", games);
 app.use("/api/teams", teams);
-
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Poker-Underground application test." });
 });
