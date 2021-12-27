cd67507cb626930c261b3e62984200a670559aa1
“nadav <nadavg1000@gmail.com>
2021-12-27 20:30:51 +0200

    edited retool to viewer

--- src/components/games/editGames.jsx
+++ src/components/games/editGames.jsx
@@ -5,7 +5,7 @@ const EditGames = () => {
     return ( 
         <div className="container">
             <PageHeader titleText="Edit Games"/>
-            <iframe title="edit games" src="https://nadav.retool.com/apps/REST%20API%20Admin%20Panel" width="100%" height="800px"></iframe>
+            <iframe src="https://nadav.retool.com/embedded/public/b9a1f778-e9c9-44f9-9309-37862ad3a349" width="100%" height="800px"></iframe>
         </div>
      );
 }
