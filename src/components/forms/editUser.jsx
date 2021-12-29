e24ae4a2eb7efa589c9ea36699cd93083a5affc2
nadavgali <nadavg@mobile-brain.net>
2021-12-29 18:32:35 +0200

    edit user

--- src/components/forms/editUser.jsx
+++ src/components/forms/editUser.jsx
@@ -6,27 +6,24 @@ import http from "../../services/httpService";
 import { apiUrl } from "../../config.json";
 import { toast } from "react-toastify";
 import userService from "../../services/userService";
-
+import { apiImage } from "../../config.json";
+import {Link} from "react-router-dom"
 
 
 
 const validationSchema = Yup.object({
   nickName: Yup.string().required("Required"),
   image: Yup.string(),
-  email: Yup.string().email("Invaild email format").required("Required"),
+
 });
 
 const EditUser = () => {
-
   const [me, setMe]=useState({})
   useEffect(()=>{
     const meData=async ()=>{
       let getUser=await userService.getUserDetails();
-      console.log(getUser,"sdsd");
       getUser=getUser.data;
       setMe(getUser);
-      console.log(me,"pp");
-
     }
     meData()
   },[])
@@ -57,7 +54,7 @@ const EditUser = () => {
 
   const initialValues = {
     nickName: me.nickName,
-    image: "",
+    image: me.image,
   };
   const [errors, setErrors] = useState({ email: "", image: "" });
   const [fields, setFields] = useState(initialValues);
@@ -74,20 +71,23 @@ const EditUser = () => {
       >
         {(formik) => {
           return (
-            <div className="row mt-4">
+            <div className="row mt-2">
               <div className="col-10 col-lg-4">
                 <Form>
                   <div className="form-control d-flex flex-column  bg-primary">
                     <label htmlFor="Nick Name">Nick Name</label>
-                    <Field type="text" id="nickName" name="nickName" />
+                    <Field type="text" id="nickName" name="nickName"  value={me.nickName}/>
                     <ErrorMessage
                       name="nickName"
                       component="div"
                       className="error"
                     />
                   </div>
+                  <div className="oldImage w-25 mb-2 ">
+                  <img src={`${apiImage}${me.image}`} alt="user" className="w-100 h-100" />
+                  </div>
                   <div className="form-control d-flex flex-column bg-primary mb-3">
-                    <label htmlFor="image">Image</label>
+                    <label htmlFor="image">Change Image</label>
                     <input
                       className="row mb-4 inputFile"
                       type="file"
@@ -98,6 +98,7 @@ const EditUser = () => {
                     ></input>
                     <span className="error">{errors.image}</span>
                   </div>
+                  <div className="buttons d-flex justify-content-between">
                   <button
                     type="submit"
                     className="btn btn-primary"
@@ -105,6 +106,15 @@ const EditUser = () => {
                   >
                     Submit
                   </button>
+                  <Link
+                type="button"
+                className="btn btn-secondary  m-0"
+                to={`/my-stats/${me._id}`}
+              >
+                Not right now
+              </Link> 
+                  </div>
+          
                 </Form>
               </div>
             </div>
