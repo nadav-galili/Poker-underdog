fda41c7efea632f8f3187efee57e58e1a33e9dd4
Nadav Galili <67638018+nadav-galili@users.noreply.github.com>
2023-10-28 19:42:57 +0300

    workflows fix

--- backend/app.js
+++ backend/app.js
@@ -18,18 +18,18 @@ const { engine } = require("express-handlebars");
 
 const config = require("./config");
 mongoose
-    .connect(
-        `mongodb+srv://${config.production.database.user_name}:` +
-            `${config.production.database.pass}@${config.production.server.cluster}/${config.production.database.db}?retryWrites=true&w=majority`,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-        }
-    )
-    .then(() => console.log("Connected to MongoDB remote..."))
-    .catch((err) => console.error(err, "Could not connect to MongoDB..."));
+  .connect(
+    `mongodb+srv://${config.production.database.user_name}:` +
+      `${config.production.database.pass}@${config.production.server.cluster}/${config.production.database.db}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("Connected to MongoDB remote..."))
+  .catch((err) => console.error(err, "Could not connect to MongoDB..."));
 
 // mongoose
 //   .connect("mongodb://localhost:27017/myFirstDatabase", {
@@ -68,7 +68,7 @@ app.use("/images", express.static(path.join(__dirname, "/images")));
 // app.use("/sounds", express.static(path.join(__dirname, "/sounds")));
 
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to Poker-Underground application test." });
+  res.json({ message: "Welcome to Poker-Underground application test." });
 });
 const PORT = process.env.PORT || 3900;
 http.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
